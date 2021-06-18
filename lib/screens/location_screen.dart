7a309526908c92cc7aa5b85c2dc4dfa9bc900164
hb57786 <tadeufcad@gmail.com>
2021-06-18 06:12:25 -0300

    Update background image

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -51,7 +51,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
+            image: AssetImage('images/city_background.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
